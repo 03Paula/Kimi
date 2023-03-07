353a1c02328850f9f2a97e546130b2bc89b06ed9
03Paula <pflogar0401@g.educaand.es>
2023-03-07 11:50:48 +0100

    cambios necesarios para el despliegue en github pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/Kimi/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
