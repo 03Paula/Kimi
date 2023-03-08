b8ecaf9dad6c645c9db8abfe9a5b6cd97067bca1
03Paula <pflogar0401@g.educaand.es>
2023-03-08 18:28:00 +0100

    intento 6

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Index from '../views/index.vue'
 import Login from '@/components/Login.vue'
-import Registro from '../components/Registro.vue'
-import Listado from '../components/Listado.vue'
-import Contacto from '../components/Contacto.vue'
-import Producto from '../components/Producto.vue'
-import Usuario from '../components/Usuario.vue'
+import Registro from '@/components/Registro.vue'
+import Listado from '@/components/Listado.vue'
+import Contacto from '@/components/Contacto.vue'
+import Producto from '@/components/Producto.vue'
+import Usuario from '@/components/Usuario.vue'
 
 
 const router = createRouter({
